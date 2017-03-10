e807768064e8d2fd0474545de29bd793b8290eb7
Colin Farquhar <cifarquhar@gmail.com>
2017-03-10 14:35:43 +0000

    add game class

---
+++ card_game/Game.java
@@ -0,0 +1,19 @@
+package card_game;
+
+import java.util.*;
+
+public class Game {
+
+  Dealer dealer;
+  ArrayList<Player> players;
+
+
+public Game(Dealer dealer, ArrayList<Player> players) {
+  this.dealer = dealer;
+  this.players = players;
+}
+
+
+
+
+}
---
+++ specs/GameTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import card_game.*;
+import java.util.*;
+
+public class GameTest{
+
+  Dealer testDealer;
+  ArrayList<Player> players;
+  Player testPlayer1;
+  Player testPlayer2;
+  CardCollection testDeck;
+  CardCollection testPlayer1Hand;
+  CardCollection testPlayer2Hand;
+  Card testCard1;
+  Card testCard2;
+  Card testCard3;
+  Card testCard4;
+  Card testCard5;
+
+  @Before
+  public void before(){
+    testDeck = new Deck();
+    testPlayer1Hand = new Hand();
+    testPlayer2Hand = new Hand();
+    testDealer = new Dealer(testDeck);
+    testPlayer1 = new Player("Player 1",testPlayer1Hand);
+    testPlayer2 = new Player("Player 2",testPlayer2Hand);
+  }
+
+
+}
