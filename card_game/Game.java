52a637253b967318ae12f89abdcb4d41dd380873
Colin Farquhar <cifarquhar@gmail.com>
2017-03-12 13:55:17 +0000

    changes made to turn functions, still not working

--- card_game/Game.java
+++ card_game/Game.java
@@ -6,13 +6,13 @@ public class Game {
 
   Dealer dealer;
   ArrayList<Player> players;
-  CardCollection cardsInPlay;
+  // public CardCollection cardsInPlay;
 
 
-public Game(Dealer dealer, CardCollection cardsInPlay) {
+public Game(Dealer dealer) {
   this.dealer = dealer;
   this.players = new ArrayList<Player>();
-  this.cardsInPlay = cardsInPlay;
+  // this.cardsInPlay = cardsInPlay;
 }
 
 public int countPlayers(){
@@ -23,10 +23,32 @@ public void addPlayer(Player player){
   players.add(player);
 }
 
-public void playTurn(Player player){
-  Card cardPlayed = player.cardsHeld.removeCard();
-  cardsInPlay.addCard(cardPlayed);
+// public void playTurn(Player player){
+//   Card cardPlayed = player.cardsHeld.removeCard();
+//   cardsInPlay.addCard(cardPlayed);
+// }
+
+public String checkWinner(){
+  int player1Score = players.get(0).cardsHeld.get(0).value();
+  int player2Score = players.get(1).cardsHeld.get(0).value;
 }
+
+
+
+
+//   if (cardsInPlay.cards(0).value > cardsInPlay.cards(1).value){
+//   return "Player 1 wins!";
+// }
+//   else if (cardsInPlay.cards(0).value < cardsInPlay.cards(1).value){
+//   return "Player 2 wins!";
+// }
+//   else if (cardsInPlay.cards(0).value == cardsInPlay.cards(1).value){
+//   return "Draw!";
+// }
+//   else {
+//     return "Something's gone wrong...";
+//   }
+// }
 
 
 
--- specs/GameTest.java
+++ specs/GameTest.java
@@ -19,7 +19,7 @@ public class GameTest{
   Card testCard4;
   Card testCard5;
   Card testCard6;
-  CardCollection testInPlay;
+  // CardCollection testInPlay;
 
   @Before
   public void before(){
@@ -29,8 +29,8 @@ public class GameTest{
     testDealer = new Dealer(testDeck);
     testPlayer1 = new Player("Player 1",testPlayer1Hand);
     testPlayer2 = new Player("Player 2",testPlayer2Hand);
-    testInPlay = new InPlay();
-    testGame = new Game(testDealer,testInPlay);
+    // testInPlay = new InPlay();
+    testGame = new Game(testDealer);
     testCard1 = new Card(CardSuit.HEARTS,5);
     testCard2 = new Card(CardSuit.CLUBS,13);
     testCard3 = new Card(CardSuit.SPADES,2);
@@ -51,15 +51,29 @@ public class GameTest{
     assertEquals(2,testGame.countPlayers());
   }
 
-  @Test
-  public void canPlayTurn(){
-    testDeck.addCard(testCard1);
-    testDeck.addCard(testCard2);
-    testGame.addPlayer(testPlayer1);
-    testDealer.dealToPlayer(testPlayer1);
-    testGame.playTurn(testPlayer1);
-    assertEquals(1,testInPlay.countCards());
-  }
+  // @Test
+  // public void canPlayTurn(){
+  //   testDeck.addCard(testCard1);
+  //   testDeck.addCard(testCard2);
+  //   testGame.addPlayer(testPlayer1);
+  //   testDealer.dealToPlayer(testPlayer1);
+  //   testGame.playTurn(testPlayer1);
+  //   assertEquals(1,testInPlay.countCards());
+  // }
+
+  // @Test
+  // public void canCheckWinner(){
+  //   testDeck.addCard(testCard1);
+  //   testDeck.addCard(testCard2);
+  //   testGame.addPlayer(testPlayer1);
+  //   testGame.addPlayer(testPlayer2);
+  //   testDealer.dealToPlayer(testPlayer1);
+  //   testDealer.dealToPlayer(testPlayer2);
+  //   testGame.playTurn(testPlayer1);
+  //   testGame.playTurn(testPlayer2);
+  //   System.out.println(testGame.cardsInPlay);
+  //   assertEquals("Player 2 wins!",testGame.checkWinner());
+  // }
 
 
 }
